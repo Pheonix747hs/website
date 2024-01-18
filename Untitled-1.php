261c803ccf7219e3a10d1c35f94f587fb2f2faf0
Pheonix747hs <94136752+Pheonix747hs@users.noreply.github.com>
2024-01-18 23:23:09 +0530

    day 2 updates

---
+++ Untitled-1.php
@@ -0,0 +1,190 @@
+<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-12">
+						<div class="row">
+							<div class="col-12" style="display: flex; align-items: center; justify-content: center; height: 100vh; margin: 0 auto; max-width: 80%;">
+								
+							</div>
+							<div class="col-12">
+								<div class="blog-comments">
+									<h2>All Comments</h2>
+									<div class="comments-body">
+										<!-- Single Comments -->
+										<div class="single-comments">
+											<div class="main">
+												<div class="head">
+													<img src="img/author1.jpg" alt="#"/>
+												</div>
+												<div class="body">
+													<h4></h4>
+													<div class="comment-meta"><span class="meta"><i class="fa fa-calendar"></i>Date</span><span class="meta"><i class="fa fa-clock-o"></i>03:38 AM</span></div>
+													<p> </p>
+													<a href="#"><i class="fa fa-reply"></i>replay</a>
+												</div>
+											</div>
+										</div>		
+										<!--/ End Single Comments -->
+										<!-- Single Comments -->
+										<div class="single-comments left">
+											<div class="main">
+												<div class="head">
+													<img src="img/author2.jpg" alt="#"/>
+												</div>
+												<div class="body">
+													<h4>Name</h4>
+													<div class="comment-meta"><span class="meta"><i class="fa fa-calendar"></i>Date</span><span class="meta"><i class="fa fa-clock-o"></i>03:38 AM</span></div>
+													<p>..</p>
+													<a href="#"><i class="fa fa-reply"></i>replay</a>
+												</div>
+											</div>
+										</div>		
+										<!--/ End Single Comments -->
+										<!-- Single Comments -->
+										<div class="single-comments">
+											<div class="main">
+												<div class="head">
+													<img src="img/author3.jpg" alt="#"/>
+												</div>
+												<div class="body">
+													<h4>Name</h4>
+													<div class="comment-meta"><span class="meta"><i class="fa fa-calendar"></i>Date</span><span class="meta"><i class="fa fa-clock-o"></i>03:38 AM</span></div>
+													<p></p>
+													<a href="#"><i class="fa fa-reply"></i>replay</a>
+												</div>
+											</div>
+										</div>		
+										<!--/ End Single Comments-->											
+									</div>
+								</div>
+							</div>
+							<div class="col-12">
+								<div class="comments-form">
+									<h2>Leave Comments</h2>
+									<!-- Contact Form -->
+									<form class="form" method="post" action="mail/mail.php">
+										<div class="row">
+											<div class="col-lg-4 col-md-4 col-12">
+												<div class="form-group">
+													<i class="fa fa-user"></i>
+													<input type="text" name="first-name" placeholder="First name" required="required">
+												</div>
+											</div>
+											<div class="col-lg-4 col-md-4 col-12">
+												<div class="form-group">
+													<i class="fa fa-envelope"></i>
+													<input type="text" name="last-name" placeholder="Last name" required="required">
+												</div>
+											</div>
+											<div class="col-lg-4 col-md-4 col-12">
+												<div class="form-group">
+													<i class="fa fa-envelope"></i>
+													<input type="email" name="email" placeholder="Your Email" required="required">
+												</div>
+											</div>
+											<div class="col-12">
+												<div class="form-group message">
+													<i class="fa fa-pencil"></i>
+													<textarea name="message" rows="7" placeholder="Type Your Message Here" ></textarea>
+												</div>
+											</div>
+											<div class="col-12">
+												<div class="form-group button">	
+													<button type="submit" class="btn primary"><i class="fa fa-send"></i>Submit Comment</button>
+												</div>
+											</div>
+										</div>
+									</form>
+									<!--/ End Contact Form -->
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-12">
+						<div class="main-sidebar">
+							<!-- Single Widget -->
+							<div class="single-widget search">
+								<div class="form">
+									<input type="email" placeholder="Search Here...">
+									<a class="button" href="#"><i class="fa fa-search"></i></a>
+								</div>
+							</div>
+							<!--/ End Single Widget -->
+							<!-- Single Widget -->
+							<div class="single-widget category">
+								<h3 class="title">Blog Categories</h3>
+								<ul class="categor-list">
+									<li><a href="#">Mutual Funds</a></li>
+									<li><a href="#">Stocks</a></li>
+									<li><a href="#">Commodities</a></li>
+									<li><a href="#">Futures</a></li>
+									<li><a href="#">Real Estate</a></li>
+								</ul>
+							</div>
+							<!--/ End Single Widget -->
+							<!-- Single Widget -->
+							<div class="single-widget recent-post">
+								<h3 class="title">Recent post</h3>
+								<!-- Single Post -->
+								<div class="single-post">
+									<div class="image">
+										<img src="img/blog-sidebar1.jpg" alt="#">
+									</div>
+									<div class="content">
+										<h5><a href="#">We have annnocuced our new product.</a></h5>
+										<ul class="comment">
+											<li><i class="fa fa-calendar" aria-hidden="true"></i>Jan 11, 2024</li>
+											<li><i class="fa fa-commenting-o" aria-hidden="true"></i>35</li>
+										</ul>
+									</div>
+								</div>
+								<!-- End Single Post -->
+								<!-- Single Post -->
+								<div class="single-post">
+									<div class="image">
+										<img src="img/blog-sidebar2.jpg" alt="#">
+									</div>
+									<div class="content">
+										<h5><a href="#">Top five way for solving Financial problems.</a></h5>
+										<ul class="comment">
+											<li><i class="fa fa-calendar" aria-hidden="true"></i>Jan 05, 2024</li>
+											<li><i class="fa fa-commenting-o" aria-hidden="true"></i>59</li>
+										</ul>
+									</div>
+								</div>
+								<!-- End Single Post -->
+								<!-- Single Post -->
+								<div class="single-post">
+									<div class="image">
+										<img src="img/blog-sidebar3.jpg" alt="#">
+									</div>
+									<div class="content">
+										<h5><a href="#">We provide highly Investment  solutions.</a></h5>
+										<ul class="comment">
+											<li><i class="fa fa-calendar" aria-hidden="true"></i>Jan 05, 2024</li>
+											<li><i class="fa fa-commenting-o" aria-hidden="true"></i>44</li>
+										</ul>
+									</div>
+								</div>
+								<!-- End Single Post -->
+							</div>
+							<!--/ End Single Widget -->
+							<!-- Single Widget -->
+							<!--/ End Single Widget -->
+							<!-- Single Widget -->
+							<div class="single-widget side-tags">
+								<h3 class="title">Tags</h3>
+								<ul class="tag">
+									<li><a href="#">business</a></li>
+									<li><a href="#">wordpress</a></li>
+									<li><a href="#">html</a></li>
+									<li><a href="#">multipurpose</a></li>
+									<li><a href="#">education</a></li>
+									<li><a href="#">template</a></li>
+									<li><a href="#">Ecommerce</a></li>
+								</ul>
+							</div>
+							<!--/ End Single Widget -->
+						</div>
+					</div>
+				</div>
+			</div>
